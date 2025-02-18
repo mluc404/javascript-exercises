5fcdb12fc869305f4fa522468e660bd418d9a917
mluc404 <199426019+mluc404@users.noreply.github.com>
2025-02-17 20:32:18 -0800

    finished 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+    let strReturn = ''
 
+    if (num < 0) return 'ERROR'
+    if (str == '') return ''
+
+    for (let i=0; i<num; i++) {
+        strReturn += str
+    }
+    return strReturn
 };
 
 // Do not edit below this line
