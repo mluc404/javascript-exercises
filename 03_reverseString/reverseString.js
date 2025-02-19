cdd0acfe7e5908f98aabc3b381e0f628d4c63b2c
mluc404 <199426019+mluc404@users.noreply.github.com>
2025-02-19 15:44:55 -0800

    Finished 3 and 4

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let strArr = str.split('')
+    let output = ''
+    for (let i=strArr.length-1; i>=0; i--) {
+        output+= strArr[i]
+    }
+    return output
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,35 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...theArgs) {
+    // let i = arr.indexOf(num)
+    // arr.splice(i,1)
+    // return arr
+    // console.log('arr:', arr)
+    // console.log('the rest: ', theArgs)
+
+
+    // solution 1: return a new array
+    // let arr2 = []
+    // for (let i in theArgs) {
+    //     console.log(theArgs[i])
+    //     arr2= arr.filter(num => num !== theArgs[i])
+    //     for (let k =0; k<3; k++) {
+    //         console.log('k: ', k)
+    //     }
+    // }
+    // console.log(arr2)
+
+    // solution 2: modify in place. Costs O(n^2) at worst
+    for (let i in theArgs) {
+        for (let k=arr.length-1; k>=0; k--) {
+            if (arr[k] === theArgs[i]) {
+                arr.splice(k,1)
+            }
+        }
+    }
+    console.log(arr)
+    return arr
 };
+
+removeFromArray([1,2,3,3], 3)
 
 // Do not edit below this line
 module.exports = removeFromArray;
