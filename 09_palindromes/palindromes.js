ea30d89c094102ef1bdf2ccdf0b5e9e4f94170fa
mluc404 <199426019+mluc404@users.noreply.github.com>
2025-02-20 16:48:51 -0800

    Finished 9: Palindromes. Learned String and Array manipulation.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,29 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    // Select only letters and numbers, and lowercase
+    str = str.replace(/[^a-zA-Z0-9]/g,'')
+    let strCleaned = str.toLowerCase()
+    console.log(strCleaned)
+
+    // // Solution1: Compare 2 Arrays
+    // arr = Array.from(strCleaned)
+    // let arrRev = arr.toReversed()
+    // return arr.every((val, ind)=> val===arrRev[ind])
+   
+    // //  Solution 2: Compare 2 Strings
+    // let str2 = arrRev.toString()
+    // let str2clean = str2.replaceAll(',','')
+    // return strCleaned===str2clean
+
+    // Most Efficient Solution:
+
+    let newStr = strCleaned.split('').reverse().join('')
+    
+    return strCleaned === newStr
 
 };
+
+// palindromes('Racecar!')
+palindromes('ZZZZ car, a man, a maracaz.')
 
 // Do not edit below this line
 module.exports = palindromes;
