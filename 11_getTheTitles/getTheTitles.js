389d72fa5273ab664771eb8f836ca9161b76eb3d
mluc404 <199426019+mluc404@users.noreply.github.com>
2025-02-21 16:26:50 -0800

    Finished 11 & 12

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,37 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(arr) {
+    let output = []
+    
+    for (let i=0; i<arr.length; i++) {
+        console.log(arr[i].title)
+        output = output.concat(arr[i].title);
+    } 
+    return output
 };
+const books = [
+    {
+      title: 'Book',
+      author: 'Name'
+    },
+    {
+      title: 'Book2',
+      author: 'Name2'
+    }
+  ]
+console.log(getTheTitles(books));
 
+// function Person(inputName) {
+//     this.name = inputName
+//     this.introduceSelf = `Hi! I'm ${this.name}.`
+//     }
+// let minh = new Person('Minh')
+// console.log(minh.introduceSelf)
+
+// let c = ['a'].concat(['b'])
+
+// console.log(typeof(c))
+// console.log(c)
+
+// let d = [1,2]
+// console.log(typeof(d))
 // Do not edit below this line
 module.exports = getTheTitles;
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,60 @@
-const findTheOldest = function() {
+const findTheOldest = function (arr) {
 
+    // Create varible for max age, initial value = 0
+    // Loop thru the array
+    // At the current index, compute the age
+    // If the current age > max age => set max age = current age; and max index = current index
+    // After the looop, return the element at the max index
+
+    let maxAge = 0;
+    let maxInd = 0;
+
+    for (let i = 0; i < arr.length; i++) {
+        let currAge = 0;
+        if (arr[i].yearOfDeath == undefined) {
+            currAge = 2025 - arr[i].yearOfBirth
+        } else {
+            currAge = arr[i].yearOfDeath - arr[i].yearOfBirth
+        }
+        if (currAge > maxAge) {
+            maxAge = currAge
+            maxInd = i
+        }
+    }
+    return arr[maxInd]
 };
+
+
+const people = [
+    {
+        name: "Carly",
+        yearOfBirth: 1942,
+        yearOfDeath: 1970,
+    },
+    {
+        name: "Ray",
+        yearOfBirth: 1962,
+        yearOfDeath: 2011,
+    },
+    {
+        name: "Jane",
+        yearOfBirth: 1912,
+        yearOfDeath: 1941,
+    },
+]
+
+console.log(findTheOldest(people).name)
+
+
+
+
+let carly = {
+    name: "Carly",
+    yearOfBirth: 1942,
+    yearOfDeath: 1970,
+};
+
+console.log(carly.age == undefined)
 
 // Do not edit below this line
 module.exports = findTheOldest;
